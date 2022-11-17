66fd0b487bba8fa856c7004cd06049b94b9f3af8
rrfactor <qrrqfactor@tip.edu.ph>
2022-11-17 03:14:15 +0000

    midterms commit

---
+++ midterm_app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+
+midterm = Flask(__name__)
+
+@midterm.route("/")
+def main():
+
+    return render_template("login.html")
+
+if __name__ == "__main__":
+    midterm.run(host="0.0.0.0", port=8080)
